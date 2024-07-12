b6ef4138f10e60e14f5dda87fa2310d3d29bc83e
titojp <titojp2002@hotmail.com>
2024-07-11 21:43:12 -0300

    updated Jul 11

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 
-let estadoAcademic = false;
-let estadoWorkExperience = false;
+let estadoAcademic = true;
+let estadoWorkExperience = true;
+let estadoLeisure = true;
 
 // Function to change the content of t2
 function modifyAcademic() {
@@ -15,12 +16,20 @@ function modifyWorkExperience() {
     estadoWorkExperience = !estadoWorkExperience;
 }
 
+function modifyLeisure() {
+    let elemento1 = document.querySelector(".containerLeisure");
+    (estadoLeisure) ? elemento1.style.display = "none" : elemento1.style.display = "contents";
+    estadoLeisure = !estadoLeisure;
+}
+
 // Function to add event listener to t
 function load() {
     var iconAcademic = document.getElementById("iconAcademic");
     iconAcademic.addEventListener("click", modifyAcademic, false);
     var iconWorkExperience = document.getElementById("iconWorkExperience");
     iconWorkExperience.addEventListener("click", modifyWorkExperience, false);
+    var iconLeisure = document.getElementById("iconLeisure");
+    iconLeisure.addEventListener("click", modifyLeisure, false);
 
 }
 
